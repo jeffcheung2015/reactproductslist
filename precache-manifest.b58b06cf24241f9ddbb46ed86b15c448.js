3a0b5303157e690dc6c2336778871693b4bc05c6
jeffcheung <jeffcheung2015@gmail.com>
2019-10-23 10:24:58 +0800

    Updates

---
+++ precache-manifest.b58b06cf24241f9ddbb46ed86b15c448.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0536c6e2aca86c137411b4de0d36a5e",
+    "url": "/reactproductslist/index.html"
+  },
+  {
+    "revision": "9e1ff172dbac799f1cb8",
+    "url": "/reactproductslist/static/css/2.4299ff6f.chunk.css"
+  },
+  {
+    "revision": "28ce31f7afc9cea0758f",
+    "url": "/reactproductslist/static/css/main.738a2069.chunk.css"
+  },
+  {
+    "revision": "9e1ff172dbac799f1cb8",
+    "url": "/reactproductslist/static/js/2.426ebbbc.chunk.js"
+  },
+  {
+    "revision": "28ce31f7afc9cea0758f",
+    "url": "/reactproductslist/static/js/main.c7a40bfb.chunk.js"
+  },
+  {
+    "revision": "7502a84121c7637a2feb",
+    "url": "/reactproductslist/static/js/runtime-main.e2114aef.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/reactproductslist/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/reactproductslist/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/reactproductslist/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/reactproductslist/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/reactproductslist/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/reactproductslist/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/reactproductslist/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/reactproductslist/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/reactproductslist/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/reactproductslist/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/reactproductslist/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/reactproductslist/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "885ec9ee00a41eb29bc9c5e3820007e7",
+    "url": "/reactproductslist/static/media/olay1.885ec9ee.webp"
+  },
+  {
+    "revision": "bccebed8d43ace9c1e2418adeeed1fb4",
+    "url": "/reactproductslist/static/media/olay2.bccebed8.webp"
+  },
+  {
+    "revision": "6cd83e2f51e3a9980edbc740fd236326",
+    "url": "/reactproductslist/static/media/olay3.6cd83e2f.webp"
+  },
+  {
+    "revision": "34e341c2cadedab6d21a490d7c48bc48",
+    "url": "/reactproductslist/static/media/olay4.34e341c2.webp"
+  },
+  {
+    "revision": "025d7454a9222d76a32922d4613e936a",
+    "url": "/reactproductslist/static/media/olay5.025d7454.webp"
+  },
+  {
+    "revision": "dea836ca7d40da763009ac4cbd650ee3",
+    "url": "/reactproductslist/static/media/olay6.dea836ca.webp"
+  },
+  {
+    "revision": "8979c0533413dcf56cbe87ad9055ccc7",
+    "url": "/reactproductslist/static/media/olay7.8979c053.webp"
+  },
+  {
+    "revision": "d76df9954f7cfbae3b417283a182150d",
+    "url": "/reactproductslist/static/media/olay8.d76df995.webp"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/reactproductslist/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/reactproductslist/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/reactproductslist/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/reactproductslist/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/reactproductslist/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea3b2dc88bd9524f1947bac8ab3d130a.js"
+  "/reactproductslist/precache-manifest.b58b06cf24241f9ddbb46ed86b15c448.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactproductslist/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
